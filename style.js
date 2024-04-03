88d8d1eda07bcce18c1c4aa011935af13327f776
omogbolahan355 <omogbolahan356@gmail.com>
2024-04-03 09:14:39 -0600

    my first commit

---
+++ style.js
@@ -0,0 +1,33 @@
+let password = "Ade";
+let attempts = 3;
+let input;
+
+let submit = document.getElementById('submit')
+
+
+
+
+function login() {
+   let username = document.getElementById('username')
+   let pass = document.getElementById('pass').value
+
+
+   let validPass = password === pass
+
+   let usernameE = username.value
+   console.log(usernameE)
+
+   if (usernameE == "" || !pass) {
+      alert("fill all filed")
+   } else if (password !== pass) {
+      alert('password not macth')
+
+   } else {
+      alert("password correct")
+      location.href = 'melodia.html'
+      alert(`welcome`)
+   }
+
+ 
+
+}    
